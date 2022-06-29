6c7692084ebd4df4f2947d7a2b322f5fd58d1a92
yangjianxiang <yangjianxiang@xunyou.com>
2022-06-29 14:32:57 +0800

    6-10 上拉加载更多与分页功能实现

--- lib/page/home_tab_page.dart
+++ lib/page/home_tab_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart' hide Banner;
 import 'package:flutter_bilibili/http/core/hi_error.dart';
 import 'package:flutter_bilibili/http/dao/home_dao.dart';
 import 'package:flutter_bilibili/model/index.dart';
+import 'package:flutter_bilibili/util/color.dart';
 import 'package:flutter_bilibili/util/hi_functions.dart';
 import 'package:flutter_bilibili/widget/hi_banner.dart';
 import 'package:flutter_bilibili/widget/video_card.dart';
@@ -18,41 +19,63 @@ class HomeTabPage extends StatefulWidget {
   State<HomeTabPage> createState() => _HomeTabPageState();
 }
 
-class _HomeTabPageState extends State<HomeTabPage> with AutomaticKeepAliveClientMixin {
+class _HomeTabPageState extends State<HomeTabPage>
+    with AutomaticKeepAliveClientMixin {
   List<Video> videoList = [];
   int pageIndex = 1;
+  bool _loading = false;
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
+    _scrollController.addListener(() {
+      var dis = _scrollController.position.maxScrollExtent -
+          _scrollController.position.pixels;
+      if (dis < 300 && !_loading) {
+        _loadData(loadMore: true);
+      }
+    });
     _loadData();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return MediaQuery.removePadding(
-      removeTop: true,
-      context: context,
-      child: StaggeredGridView.countBuilder(
-          crossAxisCount: 2,
-          itemCount: videoList.length,
-          padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-          itemBuilder: (BuildContext context, int index) {
-            if (widget.bannerList != null && index == 0) {
-              return Padding(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: _banner(),
-              );
-            }
-            return VideoCard(video: videoList[index]);
-          },
-          staggeredTileBuilder: (index) {
-            if (widget.bannerList != null && index == 0) {
-              return const StaggeredTile.fit(2);
-            }
-            return const StaggeredTile.fit(1);
-          }),
+    return RefreshIndicator(
+      onRefresh: _loadData,
+      color: primary,
+      child: MediaQuery.removePadding(
+        removeTop: true,
+        context: context,
+        child: StaggeredGridView.countBuilder(
+            controller: _scrollController,
+            crossAxisCount: 2,
+            itemCount: videoList.length,
+            padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+            physics: const AlwaysScrollableScrollPhysics(),
+            itemBuilder: (BuildContext context, int index) {
+              if (widget.bannerList != null && index == 0) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: _banner(),
+                );
+              }
+              return VideoCard(video: videoList[index]);
+            },
+            staggeredTileBuilder: (index) {
+              if (widget.bannerList != null && index == 0) {
+                return const StaggeredTile.fit(2);
+              }
+              return const StaggeredTile.fit(1);
+            }),
+      ),
     );
   }
 
@@ -63,11 +86,13 @@ class _HomeTabPageState extends State<HomeTabPage> with AutomaticKeepAliveClient
     );
   }
 
-  void _loadData({loadMore = false}) async {
+  Future<void> _loadData({loadMore = false}) async {
+    _loading = true;
     if (!loadMore) {
       pageIndex = 1;
     }
     var currentIndex = pageIndex + (loadMore ? 1 : 0);
+    hiPrint('_loadData, currentIndex = $currentIndex');
     try {
       Home result = await HomeDao.get(widget.categoryName,
           pageIndex: currentIndex, pageSize: 10);
@@ -81,15 +106,20 @@ class _HomeTabPageState extends State<HomeTabPage> with AutomaticKeepAliveClient
           videoList = result.videoList;
         }
       });
+      Future.delayed(const Duration(milliseconds: 1000), () {
+        _loading = false;
+      });
     } on NeedAuth catch (e) {
+      _loading = false;
       hiPrint(e);
       showWarnToast(e.message);
     } on HiNetError catch (e) {
+      _loading = false;
       hiPrint(e);
       showWarnToast(e.message);
     }
   }
-  
+
   @override
   bool get wantKeepAlive => true;
 }
